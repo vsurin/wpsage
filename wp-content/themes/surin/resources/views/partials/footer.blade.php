81f3aec9617595b675e599dfc4ce5bda2de19a57
VSurin <V.Surin@evnedev.com>
2018-09-10 16:35:22 +0300

    Сделал наброски утера

--- wp-content/themes/surin/resources/views/partials/footer.blade.php
+++ wp-content/themes/surin/resources/views/partials/footer.blade.php
@@ -2,4 +2,7 @@
   <div class="container">
     @php dynamic_sidebar('sidebar-footer') @endphp
   </div>
+  <div class="copiration">
+    Designed by <a href="#">tonjoostudio</a>
+  </div>
 </footer>
