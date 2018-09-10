9308fbc416fda3f376b4d3f79ba5d73eed8d4d4e
VSurin <V.Surin@evnedev.com>
2018-09-10 16:13:50 +0300

    Сделал вывод блоа после шапки

--- wp-content/themes/surin/resources/views/layouts/app.blade.php
+++ wp-content/themes/surin/resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
   <body @php body_class() @endphp>
     @php do_action('get_header') @endphp
     @include('partials.header')
+    @include('partials.header-slider')
     <div class="wrap container" role="document">
       <div class="content">
         <main class="main">
---
+++ wp-content/themes/surin/resources/views/partials/header-slider.blade.php
@@ -0,0 +1,5 @@
+<div class="heade-slider">
+    <h2>Visual Speack Loader</h2>
+    <p>This is an example of a WordPress post, you could edit this to put information about yourself or your site so readers know where you are coming from. You can create as .many posts as you like in order to share with your readers what is oin your mind</p>
+    <div>read more</div>
+</div>
