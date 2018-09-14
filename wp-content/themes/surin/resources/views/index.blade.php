efe8a2543a42e6c426e37ef723f7e70ae8bbd256
VSurin <V.Surin@evnedev.com>
2018-09-14 16:13:40 +0300

    Вывел список постов

--- wp-content/themes/surin/resources/views/index.blade.php
+++ wp-content/themes/surin/resources/views/index.blade.php
@@ -1,8 +1,14 @@
+{{--
+  Template Name: API post
+--}}
+
+@php
+  global $wp_query;
+@endphp
+
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
-
   @if (!have_posts())
     <div class="alert alert-warning">
       {{ __('Sorry, no results were found.', 'sage') }}
@@ -11,8 +17,42 @@
   @endif
 
   @while (have_posts()) @php the_post() @endphp
-    @include('partials.content-'.get_post_type())
+    <div class="api-post-conten">
+      @php
+        $classContentPost = 'api-post-conten-full';
+        $categories = \App\Controllers\App::getCategories(get_the_ID());
+      @endphp
+
+      @if(get_the_post_thumbnail_url())
+        <div class="api-post-conten-image">
+          <div style="background: url('{{ get_the_post_thumbnail_url() }}')"></div>
+        </div>
+
+        @php
+          $classContentPost = 'api-post-conten-text'
+        @endphp
+      @endif
+
+      <div class="{{ $classContentPost }}">
+        <h3>{{ get_the_title() }} </h3>
+
+        @if($categories != '')
+          <span class="api-category-post">Posted in <span>{{ $categories }}</span></span>
+        @endif
+
+        <div class="api-post-content-div">{!! \Illuminate\Support\Str::limit(get_the_content(), 470 ,'....')  !!}</div>
+        <a href="{{ get_the_permalink() }}" class="api-buttom-link"><div class="bottom-readmore">Read more</div></a>
+      </div>
+    </div>
+    <div class="api-bottom-post"></div>
+
+    <?php if( ($wp_query->current_post + 1) < ($wp_query->post_count) ) { ?>
+      <hr class="api-post-hr" size="1">
+    <?php } ?>
+
   @endwhile
 
-  {!! get_the_posts_navigation() !!}
+  {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+
+  <div class="api-bottom-post"></div>
 @endsection
