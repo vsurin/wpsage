92718316476999d10739f726140446c25ba0adec
VSurin <V.Surin@evnedev.com>
2018-09-14 14:22:10 +0300

    Вывел иконки социалки

--- wp-content/themes/surin/resources/views/partials/header.blade.php
+++ wp-content/themes/surin/resources/views/partials/header.blade.php
@@ -1,6 +1,27 @@
 <header class="header">
   <div class="container">
     <span>About My</span>
+
+    <div class="socal-icon">
+        <a href="#">
+            <div class="icon-facebook"></div>
+        </a>
+        <a href="#">
+            <div class="icon-twiter"></div>
+        </a>
+        <a href="#">
+            <div class="icon-bb"></div>
+        </a>
+        <a href="#">
+            <div class="icon-soc1"></div>
+        </a>
+        <a href="#">
+            <div class="icon-p"></div>
+        </a>
+        <a href="#">
+            <div class="icon-inst"></div>
+        </a>
+    </div>
   </div>
   <hr>
   <div class="container">
@@ -14,20 +35,22 @@
         ?>
       </p>
   </div>
-  <div class="menu">
-      <ul>
-          <li>
-              <a href="/api-post/" class="active">HOME</a>
-          </li>
-          <li>
-              <a href="#">CATEGORIES</a>
-          </li>
-          <li>
-              <a href="#">STORE</a>
-          </li>
-          <li>
-              <a href="#">OTHERS</a>
-          </li>
-      </ul>
-  </div>
 </header>
+<div class="menu">
+    <div class="container">
+        <ul>
+            <li>
+                <a href="/api-post/" class="active">HOME</a>
+            </li>
+            <li>
+                <a href="#">CATEGORIES</a>
+            </li>
+            <li>
+                <a href="#">STORE</a>
+            </li>
+            <li>
+                <a href="#">OTHERS</a>
+            </li>
+        </ul>
+    </div>
+</div>
