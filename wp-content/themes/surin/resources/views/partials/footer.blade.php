d5126a078b03e10c6cad1dfea916bac2761388f0
VSurin <V.Surin@evnedev.com>
2018-09-14 14:23:15 +0300

    Добавил места для виджетов

--- wp-content/themes/surin/resources/views/partials/footer.blade.php
+++ wp-content/themes/surin/resources/views/partials/footer.blade.php
@@ -1,6 +1,17 @@
 <footer class="content-info">
   <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+    <div class="footer-widget-1">
+      @php dynamic_sidebar('sidebar-footer') @endphp
+    </div>
+    <div class="footer-widget-2">
+      @php dynamic_sidebar('sidebar-footer-2') @endphp
+    </div>
+    <div class="footer-widget-3">
+      @php dynamic_sidebar('sidebar-footer-3') @endphp
+    </div>
+    <div class="footer-widget-4">
+      @php dynamic_sidebar('sidebar-footer-4') @endphp
+    </div>
   </div>
   <div class="copiration">
     Designed by <a href="#">tonjoostudio</a>
